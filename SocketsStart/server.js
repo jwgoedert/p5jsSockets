6faddcbc8527a289628d839e27f41d40b3a942be
jwgoedert <jwgoedert@gmail.com>
2020-11-29 21:53:26 -0600

    initializing directory

---
+++ SocketsStart/server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+let handleRequest = function (request, response) {
+    //Handle request with a response
+};
+
+let server = http.createServer(handleRequest);
+server.listen(8080);
+
