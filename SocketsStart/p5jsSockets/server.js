be9ff1e4f6210a44a71e021814f093b81b4158c2
jwgoedert <jwgoedert@gmail.com>
2020-11-29 23:41:07 -0600

    Create working server and sketch file for p5js

--- SocketsStart/p5jsSockets/server.js
+++ SocketsStart/p5jsSockets/server.js
@@ -2,18 +2,34 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 
-let handleRequest = function (request, response) {
-    fs.readFile(__dirname + '/index.html', 
-    //Callback funtion for reading
-        function(err, data){
-            if(err){
-                response.writeHead(500);
-                return response.end('Error loading index.html');    
-            }
-            response.writeHead(200);
-            response.end(data);
-        }
-    );
+let handleRequest = function (req, res) {
+  let pathname = req.url;
+
+  if (pathname == '/'){
+    pathname = '/index.html';
+  }
+
+  let ext = path.extname(pathname);
+
+  const typeExt = {
+    '.html' : 'text/html',
+    '.js' : 'text/javascript',
+    '.css': 'text/css'
+  };
+
+  let contentType = typeExt[ext] || 'text/plain';
+  
+  fs.readFile(__dirname + pathname, 
+  //Callback funtion for reading
+    function(err, data){
+      if(err){
+        res.writeHead(500);
+        return res.end('Error loading' + pathname);    
+      }
+        res.writeHead(200, {'Content-Type': contentType});
+        res.end(data);
+      }
+  );
 };
 
 let server = http.createServer(handleRequest);
